eb17dac7a32c2654b6574f3c9b5fbf096fdd1162
Toby Dylan Hocking <tdhock5@gmail.com>
2017-03-09 12:58:25 -0500

    demo8 test fails

---
+++ tests/testthat/test-demo8.R
@@ -0,0 +1,10 @@
+library(testthat)
+context("demo8")
+library(penaltyLearning)
+
+test_that("valid CV model for 8 train data", {
+  fit <- with(demo8, IntervalRegressionCV(
+    feature.mat, target.mat, min.observations=8))
+  pred.vec <- fit$predict(demo8$feature.mat)
+  expect_equal(length(pred.vec), nrow(demo8$feature.mat))
+})
