bcc9c67799a466aaea8e6daca4a18c643411eac8
Toby Dylan Hocking <tdhock5@gmail.com>
2017-03-31 13:15:55 -0400

    target interval ROC example plot

--- R/targetIntervals.R
+++ R/targetIntervals.R
@@ -66,16 +66,40 @@ targetIntervalROC <- structure(function
   target.errors[, labels := 1]
   target.errors[, possible.fp := max(fp), by=observation]
   target.errors[, possible.fn := max(fn), by=observation]
-  pred.dt <- data.table(observation, pred.log.lambda=pred)
+  pred.dt <- data.table(observation, pred.log.lambda=as.numeric(pred))
   ROChange(target.errors, pred.dt, "observation")  
 ### list describing ROC curves, same as ROChange.
 }, ex=function(){
 
-  ## TODO plot that compares ROChange and targetIntervalROC error
-  ## curves.
+  library(penaltyLearning)
+  data(neuroblastomaProcessed, envir=environment())
+
+  pid.vec <- c("1", "4")
+  chr <- 2
+  incorrect.labels <-
+    neuroblastomaProcessed$errors[profile.id%in%pid.vec & chromosome==chr]
+  pid.chr <- paste0(pid.vec, ".", chr)
+  target.mat <- neuroblastomaProcessed$target.mat[pid.chr, , drop=FALSE]
+  pred.dt <- data.table(profile.id=pid.vec, pred.log.lambda=1.5)
+  roc.list <- list(
+    labels=ROChange(incorrect.labels, pred.dt, "profile.id"),
+    targets=targetIntervalROC(target.mat, pred.dt$pred.log.lambda))
+
+  err <- data.table(incorrect=names(roc.list))[, {
+    roc.list[[incorrect]]$roc
+  }, by=incorrect]
+  library(ggplot2)
+  ggplot()+
+    ggtitle("incorrect targets is an approximation of incorrect labels")+
+    scale_size_manual(values=c(labels=2, targets=1))+
+    geom_segment(aes(
+      min.thresh, errors,
+      color=incorrect,
+      size=incorrect,
+      xend=max.thresh, yend=errors),
+                 data=err)
   
 })
-
 targetIntervalResidual <- structure(function
 ### Compute residual of predicted penalties with respect to target
 ### intervals. This function is useful for visualizing the errors in a
