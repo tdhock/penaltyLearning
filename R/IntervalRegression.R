84ec58d0d3b96d65f44639b0a49453594cb8b966
Toby Dylan Hocking <tdhock5@gmail.com>
2017-05-09 10:48:32 -0400

    try variable margin

--- R/IntervalRegression.R
+++ R/IntervalRegression.R
@@ -1,6 +1,6 @@
 ### The squared hinge loss.
-squared.hinge <- function(x){
-  ifelse(x<1,(x-1)^2,0)
+squared.hinge <- function(x, e=1){
+  ifelse(x<e,(x-e)^2,0)
 }
 
 IntervalRegressionCV <- structure(function
@@ -44,8 +44,10 @@ IntervalRegressionCV <- structure(function
 ### data.table can be computed via
 ### labelError(modelSelection(...),...)$model.errors -- see
 ### example(ROChange).
- initial.regularization=0.001
+ initial.regularization=0.001,
 ### Passed to IntervalRegressionRegularized.
+  margin.vec=NULL
+### NULL or a numeric vector of margin size hyper-parameters.
 ){
   validation.fold <- negative.auc <- threshold <- incorrect.labels <-
     variable <- value <- regularization <- folds <- status <- type <-
@@ -71,6 +73,22 @@ IntervalRegressionCV <- structure(function
   if(sum(all.finite)==0){
     stop("after filtering NA features, none remain for training")
   }
+  if(is.null(margin.vec)){
+    t.vec <- sort(target.mat[is.finite(target.mat)])
+    d.vec <- diff(t.vec)
+    pos.vec <- d.vec[0 < d.vec]
+    margin.vec <- exp(seq(
+      log(min(pos.vec)),
+      log(t.vec[length(t.vec)]-t.vec[1]),
+      l=10))
+  }
+  if(!(
+    is.numeric(margin.vec) &&
+    0 < length(margin.vec) &&
+    all(is.finite(margin.vec))
+  )){
+    stop("margin.vec must be a numeric vector of finite margin size parameters")
+  }
   validation.fold.vec <- unique(fold.vec)
   LAPPLY <- if(requireNamespace("future")){
     future::future_lapply
@@ -83,41 +101,48 @@ IntervalRegressionCV <- structure(function
     is.train <- !is.validation
     train.features <- feature.mat[is.train, all.finite, drop=FALSE]
     train.targets <- target.mat[is.train, , drop=FALSE]
-    fit <- IntervalRegressionRegularized(
-      train.features, train.targets, verbose=verbose,
-      initial.regularization=initial.regularization)
-    validation.features <- feature.mat[is.validation, , drop=FALSE]
-    pred.log.lambda <- fit$predict(validation.features)
-    validation.targets <- target.mat[is.validation, , drop=FALSE]
-    too.small <- pred.log.lambda < validation.targets[, 1]
-    too.big <- validation.targets[, 2] < pred.log.lambda
-    is.error <- too.small | too.big
-    left.term <- squared.hinge(pred.log.lambda-validation.targets[, 1])
-    right.term <- squared.hinge(validation.targets[, 2]-pred.log.lambda)
-    loss.vec <- colMeans(left.term+right.term)
-    error.vec <- colSums(is.error)
-    dt <- data.table(
-      validation.fold,
-      regularization=fit$regularization.vec,
-      squared.hinge.loss=loss.vec,
-      incorrect.intervals=error.vec)
-    if(!is.null(incorrect.labels.db)){
-      dt$negative.auc <- NA_real_
-      dt$incorrect.labels <- NA_real_
-      for(regularization.i in seq_along(fit$regularization.vec)){
-        pred.dt <- data.table(
-          pred.log.lambda=pred.log.lambda[, regularization.i])
-        pred.dt[[key(incorrect.labels.db)]] <-
-          rownames(feature.mat)[is.validation]
-        roc <- ROChange(
-          incorrect.labels.db, pred.dt, key(incorrect.labels.db))
-        dt[regularization.i, negative.auc := -roc$auc]
-        predicted.thresh <- roc$thresholds[threshold=="predicted", ]
-        dt[regularization.i, incorrect.labels := predicted.thresh$errors]
+    dt.list <- LAPPLY(margin.vec, function(margin){
+      cat(sprintf("margin=%f vfold=%d\n", margin, validation.fold))
+      fit <- IntervalRegressionRegularized(
+        train.features, train.targets, verbose=verbose,
+        margin=margin,
+        initial.regularization=initial.regularization)
+      validation.features <- feature.mat[is.validation, , drop=FALSE]
+      pred.log.lambda <- fit$predict(validation.features)
+      validation.targets <- target.mat[is.validation, , drop=FALSE]
+      too.small <- pred.log.lambda < validation.targets[, 1]
+      too.big <- validation.targets[, 2] < pred.log.lambda
+      is.error <- too.small | too.big
+      left.term <- squared.hinge(pred.log.lambda-validation.targets[, 1], 0)
+      right.term <- squared.hinge(validation.targets[, 2]-pred.log.lambda, 0)
+      loss.vec <- colMeans(left.term+right.term)
+      error.vec <- colSums(is.error)
+      dt <- data.table(
+        margin,
+        validation.fold,
+        regularization=fit$regularization.vec,
+        squared.hinge.loss=loss.vec,
+        incorrect.intervals=error.vec)
+      if(!is.null(incorrect.labels.db)){
+        dt$negative.auc <- NA_real_
+        dt$incorrect.labels <- NA_real_
+        for(regularization.i in seq_along(fit$regularization.vec)){
+          pred.dt <- data.table(
+            pred.log.lambda=pred.log.lambda[, regularization.i])
+          pred.dt[[key(incorrect.labels.db)]] <-
+            rownames(feature.mat)[is.validation]
+          roc <- ROChange(
+            incorrect.labels.db, pred.dt, key(incorrect.labels.db))
+          dt[regularization.i, negative.auc := -roc$auc]
+          predicted.thresh <- roc$thresholds[threshold=="predicted", ]
+          dt[regularization.i, incorrect.labels := predicted.thresh$errors]
+        }
       }
-    }
-    dt
+      dt
+    })
+    do.call(rbind, dt.list)
   })
+  browser()
   validation.data <- do.call(rbind, validation.data.list)
   variable.name <- if(!is.null(incorrect.labels.db)){
     "negative.auc"
@@ -220,9 +245,10 @@ IntervalRegressionCV <- structure(function
       plan(multiprocess)
     }
     set.seed(1)
-    i.train <- 1:200
+    i.train <- 1:100
     fit <- with(neuroblastomaProcessed, IntervalRegressionCV(
-      feature.mat[i.train,], target.mat[i.train,]))
+      feature.mat[i.train,], target.mat[i.train,],
+      verbose=1))
     ## When only features and target matrices are specified for
     ## training, the squared hinge loss is used as the metric to
     ## minimize on the validation set.
@@ -554,9 +580,11 @@ IntervalRegressionInternal <- function
  Lipschitz=NULL,
 ### A numeric scalar or NULL, which means to compute Lipschitz as the
 ### mean of the squared L2-norms of the rows of the feature matrix.
- verbose=2
+ verbose=2,
 ### Cat messages: for restarts and at the end if >= 1, and for every
 ### iteration if >= 2.
+ margin=1
+### Margin size hyper-parameter.
  ){
   stopifnot(is.matrix(features))
   stopifnot(is.numeric(features))
@@ -592,21 +620,21 @@ IntervalRegressionInternal <- function
   positive.part <- function(x){
     ifelse(x<0, 0, x)
   }
-  squared.hinge.deriv <- function(x){
-    ifelse(x<1,2*(x-1),0)
+  squared.hinge.deriv <- function(x,e=1){
+    ifelse(x<e,2*(x-e),0)
   }  
   calc.loss <- function(x){
     linear.predictor <- as.numeric(features %*% x)
-    left.term <- squared.hinge(linear.predictor-targets[,1])
-    right.term <- squared.hinge(targets[,2]-linear.predictor)
+    left.term <- squared.hinge(linear.predictor-targets[,1], margin)
+    right.term <- squared.hinge(targets[,2]-linear.predictor, margin)
     both.terms <- left.term+right.term
     weighted.loss.vec <- both.terms * weight.vec
     mean(weighted.loss.vec)
   }
   calc.grad <- function(x){
     linear.predictor <- as.numeric(features %*% x)
-    left.term <- squared.hinge.deriv(linear.predictor-targets[,1])
-    right.term <- squared.hinge.deriv(targets[,2]-linear.predictor)
+    left.term <- squared.hinge.deriv(linear.predictor-targets[,1], margin)
+    right.term <- squared.hinge.deriv(targets[,2]-linear.predictor, margin)
     full.grad <- features * (left.term-right.term) * weight.vec
     colSums(full.grad)/nrow(full.grad)
   }    
