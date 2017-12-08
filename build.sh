79659ee15d641afc25272ca83c0a4af1a55c8e45
Toby Dylan Hocking <tdhock5@gmail.com>
2017-12-08 11:35:53 -0500

    remove vignette

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 cd ..
 rm -rf penaltyLearning-release
 cp -r penaltyLearning penaltyLearning-release
+## remove long running tests and the packages that they use in
+## Suggests.
 grep -v PeakSegDP penaltyLearning/DESCRIPTION > penaltyLearning-release/DESCRIPTION
-pushd penaltyLearning-release/tests/testthat
-rm test-IntervalRegression.R test-labelError.R test-modelSelection.R test-peaks.R test-ROChange.R
-popd
+rm penaltyLearning-release/tests/testthat/test-IntervalRegression.R
+rm penaltyLearning-release/tests/testthat/test-labelError.R
+rm penaltyLearning-release/tests/testthat/test-modelSelection.R
+rm penaltyLearning-release/tests/testthat/test-peaks.R
+rm penaltyLearning-release/tests/testthat/test-ROChange.R
+rm -rf penaltyLearning-release/vignettes
 PKG_TGZ=$(R CMD build penaltyLearning-release|grep penaltyLearning_|sed 's/.*‘//'|sed 's/’.*//')
 R CMD check --as-cran $PKG_TGZ
